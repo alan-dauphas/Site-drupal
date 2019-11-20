a3eea746d7e18c9b3349c2ef604861ab6f7ef203
alan-dauphas <alan93110@gmail.com>
2019-11-20 11:16:21 +0100

    finalisation des modules date_conditions and email_form.

--- web/modules/custom/date_condition/src/Plugin/Condition/DateCondition.php
+++ web/modules/custom/date_condition/src/Plugin/Condition/DateCondition.php
@@ -3,6 +3,7 @@
 namespace Drupal\date_condition\Plugin\Condition;
 
 use Drupal\Core\Condition\ConditionPluginBase;
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -11,59 +12,66 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * @Condition(
  *   id = "date_condition",
- *   label = @Translation("Date"),
- *   }
+ *   label = @Translation("Date")
  * )
  *
  */
-class DateCondition extends ConditionPluginBase {
+class DateCondition extends ConditionPluginBase
+{
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
-    return $instance;
-  }
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state)
+  {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    // Sort all modules by their names.
-    $modules = system_rebuild_module_data();
-    uasort($modules, 'system_sort_modules_by_info_name');
-
-    $options = [NULL => t('Select a module')];
-    foreach ($modules as $module_id => $module) {
-      $options[$module_id] = $module->info['name'];
-    }
-
-    $form['module'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Select a module to validate'),
-      '#default_value' => $this->configuration['module'],
-      '#options' => $options,
-      '#description' => $this->t('Module selected status will be use to evaluate condition.'),
+    $form['start_date'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Start date'),
+      '#default_value' => $this->configuration['start_date'],
     ];
 
-    return parent::buildConfigurationForm($form, $form_state);
+    $form['end_date'] = [
+      '#type' => 'date',
+      '#title' => $this->t('End date'),
+      '#default_value' => $this->configuration['end_date'],
+    ];
+
+    return $form;/*j'aime Xavier*/
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-    $this->configuration['module'] = $form_state->getValue('module');
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state)
+  {
+    $this->configuration['start_date'] = $form_state->getValue('start_date');
+    $this->configuration['end_date'] = $form_state->getValue('end_date');
     parent::submitConfigurationForm($form, $form_state);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function defaultConfiguration() {
-    return ['module' => ''] + parent::defaultConfiguration();
+  public function defaultConfiguration()
+  {
+    return [
+        'start_date' => '',
+        'end_date' => '',
+      ] + parent::defaultConfiguration();
   }
+
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state)
+  {
+    if (!empty($form_state->getValue('start_date')) && !empty($form_state->getValue('end_date'))){
+      $start = new DrupalDateTime($form_state->getValue('start_date'));
+      $end = new DrupalDateTime($form_state->getValue('end_date'));
+      if ($end < $start) {
+        $form_state->setErrorByName('end date', $this->t('End date Error!'));
+      }
+    }
+  }
+
 
   /**
    * Evaluates the condition and returns TRUE or FALSE accordingly.
@@ -71,27 +79,26 @@ class DateCondition extends ConditionPluginBase {
    * @return bool
    *   TRUE if the condition has been met, FALSE otherwise.
    */
-  public function evaluate() {
-    if (empty($this->configuration['module']) && !$this->isNegated()){
-        return TRUE;
-    }
+  public function evaluate()
+  {
+    $today = new DrupalDateTime('today');
+    $start = $this->configuration['start_date'] ? new DrupalDateTime($this->configuration['start_date']) : NULL;
+    $end = $this->configuration['end_date'] ? new DrupalDateTime($this->configuration['end_date']) : NULL;
 
-    $module = $this->configuration['module'];
-    $modules = system_rebuild_module_data();
-
-    return $modules[$module]->status;
+    return (!$start || ($start <= $today)) && (!$end || ($end >= $today));
   }
 
   /**
    * Provides a human readable summary of the condition's configuration.
    */
-  public function summary() {
+  public function summary()
+  {
     $module = $this->getContextValue('module');
     $modules = system_rebuild_module_data();
 
-    $status = ($modules[$module]->status)?t('enabled'):t('disabled');
+    $status = ($modules[$module]->status) ? t('enabled') : t('disabled');
 
     return t('The module @module is @status.', ['@module' => $module, '@status' => $status]);
   }
-
 }
+
---
+++ web/modules/custom/email_form/src/Form/EmailForm.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\email_form\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\reusable_forms\Form\ReusableFormBase;
+
+
+/**
+ * Defines the EmailForm class.
+ */
+class EmailForm extends ReusableFormBase {
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getFormId() {
+    return 'email_form';
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['email'] = array(
+      '#type' => 'email',
+      '#title' => $this->t('Enter your email adress'),
+    );
+
+    $form = parent::buildForm($form, $form_state);
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $node = \Drupal::service('current_route_match')->getParameter('node');
+    \Drupal::database()->insert('email_form_node_subscriber')->fields([
+      'nid' => $node->id(),
+      'email' => $form_state->getValue('email'),
+    ])->execute();
+  }
+
+}
